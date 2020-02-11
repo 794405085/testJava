1b08847f2e3a069d959efe278864e3aeaaa4a3ae
794405085 <794405085@qq.com>
2020-02-11 14:06:56 +0800

    测试使用git

---
+++ testGit/src/main/java/cn/test/xj/TestGit.java
@@ -0,0 +1,13 @@
+package cn.test.xj;
+
+/**
+ * @ClassName TestGit
+ * @Description: 测试使用github
+ * @Author 79440
+ * @Date 2020/2/11
+ * @Version V1.0
+ **/
+public class TestGit {
+    private String name;
+    private int id;
+}
